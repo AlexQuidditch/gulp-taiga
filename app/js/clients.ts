7d6348e55aa302079e3a5d53d50a6023db4485b6
AlexQuidditch <alexquidditch@gmail.com>
2017-03-10 17:43:01 +0700

    added clients page

---
+++ app/js/clients.ts
@@ -0,0 +1,17 @@
+'use strict'
+
+document.addEventListener("DOMContentLoaded", () => {
+	const playerSlider = new Flickity('#playerSlider', {
+		cellSelector: '.player-item',
+		wrapAround: !0,
+		contain: !0,
+		arrowShape: { x0: 25, x1: 60, y1: 40, x2: 65, y2: 35, x3: 35 }
+	});
+
+	const clientsSlider = new Flickity('#clientsSlider', {
+		cellSelector: '.clients-item',
+		wrapAround: !0,
+		arrowShape: { x0: 25, x1: 60, y1: 40, x2: 65, y2: 35, x3: 35 }
+	});
+
+});
