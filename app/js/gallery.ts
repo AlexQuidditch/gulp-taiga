9a81b58336c848ed5515565222cfe1b6a1bf1ef9
AlexQuidditch <alexquidditch@gmail.com>
2017-03-17 03:52:58 +0700

    added upload modal window

--- app/js/gallery.ts
+++ app/js/gallery.ts
@@ -9,8 +9,17 @@ document.addEventListener("DOMContentLoaded", () => {
 		bgLazyLoad: !0,
 		prevNextButtons: 0
 	});
-
 	document.getElementById('newsNext').addEventListener('click', () => newsSlider.next(!0, 0));
 	document.getElementById('newsPrev').addEventListener('click', () => newsSlider.previous(!0, 0));
+
+	document.getElementById('addContent').addEventListener('click', () => {
+		document.getElementById('overlay').classList.add('_opened');
+	});
+
+	function getFileName() {
+		let file:string = document.getElementById('uploaded-file').value;
+		file = file.replace(/\\/g, "/").split("/\ / ").pop();
+		document.getElementById('file-name').innerHTML = `Имя файла: ${file}`;
+	};
 
 });
